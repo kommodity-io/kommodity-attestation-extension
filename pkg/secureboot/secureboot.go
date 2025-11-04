4dbf3f6f48587cfa00b140a4fd166b36800c769c
Steffen Karlsson <steffen.karlsson@gmail.com>
2025-11-04 16:25:45 +0100

    feat(secureboot): implement helper to check secureboot (#4)
    Signed-off-by: Steffen Karlsson <steffen.karlsson@gmail.com>

---
+++ pkg/secureboot/secureboot.go
@@ -0,0 +1,23 @@
+// Package secureboot provides secure boot information for talos machine.
+package secureboot
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	efiVarsDir        = "/sys/firmware/efi/efivars"
+	efiGlobalVarGUID  = "8be4df61-93ca-11d2-aa0d-00e098032b8c" // EFI_GLOBAL_VARIABLE
+	secureBootVarPath = efiVarsDir + "/SecureBoot-" + efiGlobalVarGUID
+)
+
+func IsSecureBootEnabled() (bool, error) {
+	data, err := os.ReadFile(secureBootVarPath)
+	if err != nil {
+		return false, fmt.Errorf("failed to read SecureBoot variable: %w", err)
+	}
+
+	// SecureBoot value is the last byte; 1 means enabled, 0 means disabled
+	return data[len(data)-1] == 1, nil
+}
